1e190f15ce96678aa31c6a4a0c233e35e8deb8ec
Artem Korolev <artkor@live.ru>
2021-11-09 12:46:30 +0200

    Updates

--- ghspa.js
+++ ghspa.js
@@ -51,5 +51,5 @@
 	/* if current document is 404 page page, redirect to index.html otherwise resolve */
 	document.title === '404' ? redirect() : resolve()
   
-  }(window.location, window.projectPages || true ))
+  }(window.location, window.projectPages || false ))
   
