edd9addf313e4d1ecdedfaf78b0cc6eb53a04519
Artem Korolev <artkor@live.ru>
2021-11-10 02:09:26 +0200

    fix(website): code formatting

--- packages/website/src/pages/home/HomeComponent.js
+++ packages/website/src/pages/home/HomeComponent.js
@@ -15,11 +15,11 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <img alt="design" style={ DesignStyles } width="100%" src={require(`../../assets/design/197499459_753257925348887_9065983798484886948_n.png`)} />
-        <img alt="design" style={ DesignStyles } width="100%" src={require(`../../assets/design/198709469_2993284834282728_5567266784561756959_n.png`)} />
-        <img alt="design" style={ DesignStyles } width="100%" src={require(`../../assets/design/198829890_2979752505681533_7593360201138606489_n.png`)} />
-        <img alt="design" style={ DesignStyles } width="100%" src={require(`../../assets/design/199637576_440337583986891_3187947300306788917_n.png`)} />
-        <img alt="design" style={ DesignStyles } width="100%" src={require(`../../assets/design/200087697_328336922091626_941398468522586669_n.png`)} />
+        <img alt="design" style={DesignStyles} width="100%" src={require(`../../assets/design/197499459_753257925348887_9065983798484886948_n.png`)} />
+        <img alt="design" style={DesignStyles} width="100%" src={require(`../../assets/design/198709469_2993284834282728_5567266784561756959_n.png`)} />
+        <img alt="design" style={DesignStyles} width="100%" src={require(`../../assets/design/198829890_2979752505681533_7593360201138606489_n.png`)} />
+        <img alt="design" style={DesignStyles} width="100%" src={require(`../../assets/design/199637576_440337583986891_3187947300306788917_n.png`)} />
+        <img alt="design" style={DesignStyles} width="100%" src={require(`../../assets/design/200087697_328336922091626_941398468522586669_n.png`)} />
       </div>
     );
   }
